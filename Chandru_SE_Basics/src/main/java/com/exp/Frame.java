ea498798591dae81817bf76fedaf7911d018c451
ChandruJ <2k21it08@kiot.ac.in>
2025-03-17 16:01:40 +0530

    updated

---
+++ Chandru_SE_Basics/src/main/java/com/exp/Frame.java
@@ -0,0 +1,28 @@
+package com.exp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Frame {
+	public static void main(String args[]) {
+		ChromeOptions options=new ChromeOptions();
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://demoqa.com/frames");
+		WebElement iframeElement = driver.findElement(By.id("frame1"));
+		driver.switchTo().frame(iframeElement);
+		String Frame_1=driver.findElement(By.id("sampleHeading")).getText();
+		System.out.println("Switch by locator:"+Frame_1);
+		driver.switchTo().defaultContent();
+		String mainPage= driver.findElement(By.xpath("//*[@id=\"framesWrapper\"]/h1")).getText();
+		System.out.println("Back to Main page:"+mainPage);
+		driver.switchTo().frame(3);
+		String Frame_2 = driver.findElement(By.id("sampleHeading")).getText();
+		System.out.println("Switch by index:"+Frame_2);
+		driver.quit();
+	}
+
+}
---
+++ Chandru_SE_Basics/src/main/java/com/exp/seleniumpracticedate.java
@@ -0,0 +1,23 @@
+package com.exp;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class seleniumpracticedate {
+     public static void main(String args[]) {
+    	 ChromeOptions options = new ChromeOptions();
+ 		WebDriver driver = new ChromeDriver();
+         driver.manage().window().maximize();
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+         driver.get("https://seleniumpractise.blogspot.com/2016/08/how-to-handle-calendar-in-selenium.html");
+         WebElement send = driver.findElement(By.xpath("//*[@id=\"datepicker\"]"));
+         send.click();
+         
+         
+     }
+}
