3ab0350166fc3b08282e9f0633191a1ace212a13
ChandruJ <2k21it08@kiot.ac.in>
2025-04-07 20:16:12 +0530

    commit

---
+++ Demo/src/test/java/Runner/RunnerClass.java
@@ -0,0 +1,14 @@
+package Runner;
+
+
+import org.testng.annotations.DataProvider;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json"}, features = "src/test/resources/com/feature/login.feature",
+glue = {"Step"}, monochrome = true, dryRun = false)
+public class RunnerClass  extends AbstractTestNGCucumberTests{
+
+}
